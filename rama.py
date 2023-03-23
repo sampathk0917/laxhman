cf5fd74e78b223b09cdf67e9236f00de0d005c64
sampathk0917 <sampathk0917@email.com>
2023-03-23 14:34:16 -0400

    sita file is updated

---
+++ rama.py
@@ -0,0 +1,14 @@
+a=int(input("Enter the value a:"))
+b=int(input("Enter the value b:"))
+c=int(input("Enter the value c:"))
+if a>b:
+    if a>c:
+        print("a is greater")
+    else:
+        print("c is greater")
+else:
+    if b>c:
+        print("b is greater")
+    else:
+        print("c is greater")
+        
